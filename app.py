fee5be348de4530298286bf10e8c5157e3240c5e
jiale0329 <68922276+jiale0329@users.noreply.github.com>
2023-12-10 13:21:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/recommend_product', methods=['POST'])
-def hello():
-    return 'Hello, World!'
+@app.route('/recommend_product', methods=['GET'])
+def recommend_product():
+    try:
+        # Your actual processing logic should go here
+        # ...
+
+        # For demonstration purposes, let's assume some processing and a successful response
+        response = {
+            'message': 'Recommendation generated successfully.',
+            'status': 'success'
+        }
+
+        return jsonify(response), 200
+
+    except Exception as e:
+        # Handle exceptions or errors
+        response = {
+            'message': 'Error processing the request: {}'.format(str(e)),
+            'status': 'error'
+        }
+
+        return jsonify(response), 500
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
+
